52fa0b95d6ac58c27c5408b14b4e2ba5afd60554
sahilkhatri <sahilk.khatri@gmail.com>
2023-03-12 15:27:46 +0530

    gnome terminal updated

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -51,23 +51,38 @@ public class Calculator {
 
             switch (choice) {
                 case 1:
-                    // do Multiplication
+                    // do Square root
                     System.out.print("Enter the first number : ");
                     number1 = scanner.nextDouble();
-                    System.out.print("Enter the second number : ");
-                    number2 = scanner.nextDouble();
-                    System.out.println("Multiplication of "+number1+" and "+number2+" is : " + calculator.multiplication(number1,number2));
+                    System.out.println("Square root of "+number1+" is : " + calculator.sqrt(number1));
                     System.out.println("\n");
-
                     break;
+
                 case 2:
-                    // find cube root
-                    System.out.print("Enter a number : ");
+                    // do Factorial
+                    System.out.print("Enter the first number : ");
                     number1 = scanner.nextDouble();
-                    System.out.println("Cube root of "+number1+" is : " + calculator.cuberoot(number1));
+                    System.out.println("Factorial of "+number1+" is : " + calculator.factorial(number1));
                     System.out.println("\n");
-
                     break;
+
+//                case 1:
+//                    // do Multiplication
+//                    System.out.print("Enter the first number : ");
+//                    number1 = scanner.nextDouble();
+//                    System.out.print("Enter the second number : ");
+//                    number2 = scanner.nextDouble();
+//                    System.out.println("Multiplication of "+number1+" and "+number2+" is : " + calculator.multiplication(number1,number2));
+//                    System.out.println("\n");
+//                    break;
+//                case 2:
+//                    // find cube root
+//                    System.out.print("Enter a number : ");
+//                    number1 = scanner.nextDouble();
+//                    System.out.println("Cube root of "+number1+" is : " + calculator.cuberoot(number1));
+//                    System.out.println("\n");
+//
+//                    break;
 //                case 3:
 //                    // find square
 //                    System.out.print("Enter a number : ");
@@ -122,5 +137,33 @@ public class Calculator {
         return result;
     }
 
+    public double sqrt(double number1) {
+        logger.info("[SQUARE ROOT - " + number1);
+        double sqr = Math.sqrt(number1);
+        double result = sqr;
+        logger.info("[RESULT - SQUARE ROOT - " + result);
+        return result;
+    }
+
+    public double factorial(double number1){
+        logger.info("[FACTORIAL - " + number1);
+        double result = (number1==1 || number1==0) ? 1:number1*factorial(number1-1);
+        logger.info("RESULT - FACTORIAL - " + result);
+        return result;
+    }
+
+    public double naturallog(double number1){
+        logger.info("[NATURAL LOGARITHM - " + number1);
+        double result = Math.log(number1);
+        logger.info("[RESULT - NATURAL LOGARITHM - " + result);
+        return result;
+    }
+
+    public double power(double number1, double number2){
+        logger.info("[POWER FUNCTION - " + number1 + "RAISED TO THE POWER" + number2);
+        double result = Math.pow(number1, number2);
+        logger.info("[RESULT - POWER FUNCTION - " + result);
+        return result;
+    }
 
 }
